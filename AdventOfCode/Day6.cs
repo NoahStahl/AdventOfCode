dfb5db3db42a3ac7a1b4233c8826e116e20464e2
Noah Stahl <93164708+noah-osso@users.noreply.github.com>
2021-12-06 22:12:55 -0600

    Add day 6 solution

---
+++ AdventOfCode/Day6.cs
@@ -0,0 +1,75 @@
+﻿namespace AdventOfCode;
+
+public static class Day6
+{
+    public static long RunPart1(List<int> initialTimers)
+    {
+        return Simulate(initialTimers, 80);
+    }
+
+    public static long RunPart2(List<int> initialTimers)
+    {
+        return Simulate(initialTimers, 256);
+    }
+
+    public static (long answer1, long answer2) Run()
+    {
+        string input = File.ReadAllText(@"Inputs\Day6.txt");
+        List<int> initialTimers = input.ParseNumbers(',');
+        return (RunPart1(initialTimers), RunPart2(initialTimers));
+    }
+
+    static long Simulate(List<int> initialTimers, int days)
+    {
+        Span<long> cohorts = stackalloc long[9];
+        foreach (int timer in initialTimers)
+        {
+            cohorts[timer]++;
+        }
+
+        do
+        {
+            long spawnCount = cohorts[0];
+            for (int i = 1; i <= 8; i++)
+            {
+                cohorts[i - 1] = cohorts[i];
+            }
+
+            cohorts[6] += spawnCount; 
+            cohorts[8] = spawnCount;
+        } 
+        while (--days > 0);
+
+        long total = 0;
+        for (int i = 0; i <= 8; i++)
+        {
+            total += cohorts[i];
+        }
+
+        return total;
+    }
+
+    static List<int> ParseNumbers(this string line, char delimiter)
+    {
+        List<int> values = new(line.Length / 2);
+        for (int i = 1, d = 0; i < line.Length;)
+        {
+            if (line[i] == delimiter)
+            {
+                int start = d > 0 ? d + 1 : 0;
+                if (i > start)
+                {
+                    values.Add(int.Parse(line.AsSpan(start, i - start)));
+                }
+                d = i;
+            }
+
+            if (++i == line.Length)
+            {
+                values.Add(int.Parse(line.AsSpan(d + 1)));
+            }
+        }
+
+        return values;
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -3,7 +3,7 @@ using BenchmarkDotNet.Running;
 
 Console.WriteLine("Hello, Advent of Code!");
 
-(int answer1, int answer2) = Day5.Run();
+(long answer1, long answer2) = Day6.Run();
 
 Console.WriteLine($"Answer 1: {answer1}, Answer 2: {answer2}");
 
